2c805c3a74aefddcb71fb6c497b9a53b838be235
marlio <jitao.liu82@gmail.com>
2020-08-09 10:01:21 +0800

    append source code

---
+++ src/main/java/com/wiz/example/demo/boot/HelloGradleController.java
@@ -0,0 +1,20 @@
+package com.wiz.example.demo.boot;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloGradleController {
+    private Log logger = LogFactory.getLog(HelloGradleController.class);
+
+    @GetMapping
+    public String helloGradle(String name) {
+        logger.debug("===> Input name : " + name);
+        String result = StringUtils.hasText(name) ? ("Hello, " + name + "!") : "Hello, Gradle!";
+        logger.info("RESTful Result : " + result);
+        return result;
+    }
+}

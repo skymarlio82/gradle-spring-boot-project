6e590adba885608782bb432903e9e0c87b9277cc
marlio <jitao.liu82@gmail.com>
2020-08-10 11:26:57 +0800

    shell command

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Stop Procedure : gradle-spring-boot-project-0.1.0-SNAPSHOT.jar"
-pid=`ps -ef | grep java | gradle-spring-boot-project-0.1.0-SNAPSHOT.jar | awk '{print $2}'`
+pid=`ps -ef | grep java | grep gradle-spring-boot-project-0.1.0-SNAPSHOT.jar | awk '{print $2}'`
 echo 'old Procedure pid:'$pid
 if [ -n "$pid" ]
 then

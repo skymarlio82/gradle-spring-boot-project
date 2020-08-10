6a8b345e4bc19bc9fbc6d31762ff5b36758da9d9
marlio <jitao.liu82@gmail.com>
2020-08-10 11:42:08 +0800

    updated recently.

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ echo 'Start the program : gradle-spring-boot-project-0.1.0-SNAPSHOT.jar'
 chmod 777 /root/development/github/gradle-spring-boot-project/build/libs/gradle-spring-boot-project-0.1.0-SNAPSHOT.jar
 echo '--------Starting--------' 
 cd /root/development/github/gradle-spring-boot-project/build/libs/
-nohup java -jar gradle-spring-boot-project-0.1.0-SNAPSHOT.jar &
+java -jar gradle-spring-boot-project-0.1.0-SNAPSHOT.jar &
 echo 'start success'
